82f23dbf03d5a1029ba46d3d69d528fbb6484d5d
EwanMcA <ewan.m@hotmail.com>
2024-10-11 11:48:21 +0100

    handle repeated calls by logging what we've copied

--- parse.py
+++ parse.py
@@ -1,25 +1,59 @@
 import json
 from datetime import datetime
 
-def main():
+def read_config():
     with open('config.json') as f:
-        config = json.load(f)
+        return json.load(f)
 
+
+def read_today(tags):
     today = datetime.now().strftime('%b_%d_%y.md')
-
     additions = {}
     with open(today) as f:
         for line in f:
-            for tag in config.get('tags', []):
+            for tag in tags:
                 if line.startswith(f"[{tag}]"):
                     if tag in additions:
-                        additions[tag] += f"\n{line}"
+                        additions[tag].append(line)
                     else:
-                        additions[tag] = line
+                        additions[tag] = [line]
 
-    for tag in config.get('tags', []):
-        with open(f"{tag}.md", 'a') as f:
-            f.write(additions[tag])
+    return additions
+
+
+def filter_tags(additions):
+    try:
+        with open('.log', "r+") as f:
+            loglines = f.readlines()
+    except FileNotFoundError:
+        return
+
+    for tag in additions:
+        for add in additions[tag]:
+            if add in loglines:
+                additions[tag].remove(add)
+
+
+def write_tags(additions):
+    for tag in additions:
+        with open(f"{tag}.md", 'a+') as f:
+            for add in additions[tag]:
+                f.write(add)
+
+
+def write_log(additions):
+    with open('.log', 'a+') as f: 
+        for tag in additions:
+            for add in additions[tag]:
+                f.write(add)
+
+def main():
+    config = read_config()
+    tags = config.get('tags', [])
+    additions = read_today(tags)
+    filter_tags(additions)
+    write_tags(additions)
+    write_log(additions)
                     
 
 if __name__ == '__main__':
