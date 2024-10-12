b2ca4d64db78ff2a0ef78bfac92515dd9d19db35
Ewan <ewan.m@hotmail.com>
2024-10-12 22:01:34 +0100

    use env var to target files, tighten up copying logic

--- parse.py
+++ parse.py
@@ -1,15 +1,18 @@
 import json
+import os
+import sys
+import re
 from datetime import datetime
 
-def read_config():
-    with open('config.json') as f:
+
+def read_config(dir):
+    with open(f"{dir}/config.json") as f:
         return json.load(f)
 
 
-def read_today(tags):
-    today = datetime.now().strftime('%b_%d_%y.md')
+def read_today(fn, tags):
     additions = {}
-    with open(today) as f:
+    with open(fn) as f:
         for line in f:
             for tag in tags:
                 if line.startswith(f"[{tag}]"):
@@ -21,46 +24,56 @@ def read_today(tags):
     return additions
 
 
-def filter_tags(additions):
+def filter_tags(dir, additions):
     try:
-        with open('.log', "r+") as f:
+        with open(f"{dir}/.log", "r+") as f:
             loglines = f.readlines()
     except FileNotFoundError:
         return
 
+    removals = {}
     for tag in additions:
         for add in additions[tag]:
             if add in loglines:
-                additions[tag].remove(add)
+                if tag not in removals:
+                    removals[tag] = [add]
+                else:
+                    removals[tag].append(add)
+
+    for tag in removals:
+        for add in removals[tag]:
+            additions[tag].remove(add)
 
 
 def write_tags(additions):
     # if file doesn't exist yet, add a heading
     for tag in additions:
         with open(f"{tag}.md", 'a+') as f:
-            if f.read() == '':
+            if f.tell() == 0:
                 f.write(f"# {tag}\n\n")
-
-    for tag in additions:
-        with open(f"{tag}.md", 'a') as f:
             for add in additions[tag]:
                 f.write(add)
 
 
-def write_log(additions):
-    with open('.log', 'a+') as f: 
+def write_log(dir, additions):
+    with open(f"{dir}/.log", 'a+') as f:
         for tag in additions:
             for add in additions[tag]:
                 f.write(add)
 
-def main():
-    config = read_config()
+def main(fn):
+    dir = os.environ.get("TODAY_DIR")
+    if not dir:
+        raise ValueError("TODAY_DIR not set")
+    dir = re.sub(r'/$', '', dir)
+
+    config = read_config(dir)
     tags = config.get('tags', [])
-    additions = read_today(tags)
-    filter_tags(additions)
+    additions = read_today(fn, tags)
+    filter_tags(dir, additions)
     write_tags(additions)
-    write_log(additions)
+    write_log(dir, additions)
                     
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
